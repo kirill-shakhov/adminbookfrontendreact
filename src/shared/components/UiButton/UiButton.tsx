62d3bb63e62c4efad63a05f33666c4f2cc451735
kirill <checkster98@gmail.com>
2024-07-17 18:39:37 +0400

    master feat( UiButton ) added variant for showing ( a/button )

--- src/shared/components/UiButton/UiButton.tsx
+++ src/shared/components/UiButton/UiButton.tsx
@@ -1,13 +1,31 @@
 import React, {FC} from 'react';
-import {UiButtonProps} from "./UiButton.types.ts";
+import {UiButtonProps} from "./index.ts";
 
-
-const UiButton: FC<UiButtonProps> = ({children, ...props}) => {
+const UiButton: FC<UiButtonProps> = ({children, href, disabled, type, ...props}) => {
     return (
-        <button
-            className={"flex justify-center items-center  px-3 py-1.5  font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"} {...props}>
-            {children}
-        </button>
+        <>
+
+            {
+                href ? (
+                    <a
+                        className={"flex justify-center items-center  px-3 py-1.5  font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"}{...props}
+                        href={href}
+                        target={'_blank'}
+                    >
+                        {children}
+                    </a>
+                ) : (
+                    <button
+                        className={"flex justify-center items-center  px-3 py-1.5  font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"} {...props}
+                        type={type ? type : 'button'}
+                        disabled={disabled}
+                    >
+                        {children}
+                    </button>
+                )
+            }
+
+        </>
     );
 }
 
