c6861222213fb0ec0d7f4f388c094651d05b036f
kirill <checkster98@gmail.com>
2024-07-19 19:36:10 +0400

    master feat(UiButton): added new props

--- src/shared/components/UiButton/UiButton.tsx
+++ src/shared/components/UiButton/UiButton.tsx
@@ -1,36 +1,58 @@
-import React, {FC} from 'react';
+import  {FC} from 'react';
 import {UiButtonProps} from "./index.ts";
 import {UiProgressCircular} from "../UiProgressCircular";
 
-const UiButton: FC<UiButtonProps> = ({children, href, loading, disabled, type, ...props}) => {
+import {useUiButton} from "./useUiButton.ts";
+
+
+const UiButton: FC<UiButtonProps> =
+  ({
+     children,
+     href,
+     loading,
+     disabled,
+     type,
+     size = 'sm',
+     block = false,
+     theme = 'primary',
+     ...props
+   }) => {
+
+    const {rootClasses} = useUiButton({
+      loading,
+      disabled,
+      block,
+      size: size as 'sm' | 'md' | 'lg',
+      theme: theme as 'danger' | 'primary' | 'secondary'
+    });
 
     return (
-        <>
+      <>
+        {
+          href ? (
+            <a
+              className={rootClasses}
+              href={href}
+              target={'_blank'}
+              {...props}
+            >
+              {children}
+            </a>
+          ) : (
+            <button
+              className={rootClasses}
+              type={type ? type : 'button'}
+              disabled={disabled || loading}
+              {...props}
+            >
 
-            {
-                href ? (
-                    <a
-                        className={"flex justify-center items-center  px-3 py-1.5  font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"}
-                        href={href}
-                        target={'_blank'}
-                        {...props}
-                    >
-                        {children}
-                    </a>
-                ) : (
-                    <button
-                        className={"flex justify-center items-center  px-3 py-1.5  font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"} {...props}
-                        type={type ? type : 'button'}
-                        disabled={disabled || loading}
-                    >
+              {loading ? (<UiProgressCircular/>) : children}
+            </button>
+          )
+        }
 
-                        {loading ? (<UiProgressCircular/>) : children}
-                    </button>
-                )
-            }
-
-        </>
+      </>
     );
-}
+  }
 
 export default UiButton;
