a35ccb6c6a202c408b4930febe43cba7babd9e7a
kirill <checkster98@gmail.com>
2024-07-21 12:05:00 +0400

    master feat(UiInput): added classes for label

--- src/shared/components/UiInput/UiInput.tsx
+++ src/shared/components/UiInput/UiInput.tsx
@@ -1,33 +1,53 @@
 import {FC} from "react";
 import {UiInputProps} from "./UiInput.types.ts";
 import {useUiInput} from "./useUiInput.tsx";
+// import {ExclamationCircleIcon} from '@heroicons/react/24/solid'
 
 const UiInput: FC<UiInputProps> =
   ({
+     label,
      name,
      type,
      placeholder,
      autoComplete,
      readOnly,
-     disabled
+     disabled,
+     value,
+     onChange,
+     onBlur,
+     errors,
+     touched
    }) => {
 
-    const {rootClasses} = useUiInput({readOnly, disabled});
+    const {rootClasses} = useUiInput({readOnly, disabled, errors, touched});
 
     return (
       <>
-        <input
-          type={type}
-          name={name}
-          placeholder={placeholder}
-          autoComplete={autoComplete}
-          readOnly={readOnly}
-          disabled={disabled}
-          className={rootClasses}
-        />
+        {label && <label className={"block text-sm font-medium leading-6 text-gray-900"} htmlFor={name}>{label}</label>}
 
-        <div>
-          {/*{errors.password && touched.password && errors.password}*/}
+        <div className={'ui-input-wrap relative mt-2'}>
+          <input
+            type={type}
+            name={name}
+            placeholder={placeholder}
+            autoComplete={autoComplete}
+            readOnly={readOnly}
+            disabled={disabled}
+            value={value}
+            onChange={onChange}
+            onBlur={onBlur}
+            className={rootClasses}
+          />
+
+
+          {/*{errors ? <div className={'pr-3 items-center flex right-0 top-0 bottom-0 absolute pointer-events-none'}>*/}
+          {/*  <ExclamationCircleIcon className="size-6 text-red-500"/>*/}
+          {/*</div> : ''}*/}
+        </div>
+
+
+        <div className={"text-red-500"}>
+          {errors && touched && errors}
         </div>
       </>
     );
