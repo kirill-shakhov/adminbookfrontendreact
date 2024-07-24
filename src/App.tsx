3c76955bb90b4ee785d56a6748b9f2d9ef719f96
kirill <checkster98@gmail.com>
2024-07-24 18:14:34 +0400

    master feat(App): added links

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,17 @@
 import './App.scss'
-import { Formik, Form } from "formik";
-import * as Yup from "yup";
-import {UiInput} from "./shared/components/UiInput";
-import {UiButton} from "./shared/components/UiButton";
+import {Link} from "react-router-dom";
 
 
-const validationSchema = Yup.object({
-  email: Yup.string().email("Invalid email address").required("Required"),
-  password: Yup.string().required("Required"),
-});
+const App = () => {
 
-const App: React.FC = () => (
-  <div>
-    <h1>Anywhere in your app!</h1>
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      validationSchema={validationSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => (
-        <Form onSubmit={handleSubmit}>
-          <UiInput
-            label={'email'}
-            type="email"
-            name="email"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-            errors={errors.email}
-            touched={touched.email}
-          />
+  return (
+    <div>
+      <div><Link to={`/`}>main</Link></div>
+      <div><Link to={`register`}>registration</Link></div>
+      <div><Link to={`login`}>login</Link></div>
 
-          <UiInput
-            label={'password'}
-            type="password"
-            name="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-            errors={errors.password}
-            touched={touched.password}
-          />
-
-          <UiButton
-          type={'submit'}
-          disabled={isSubmitting}
-          >
-            Submit
-          </UiButton>
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
+    </div>
+  )
+};
 
 export default App;
