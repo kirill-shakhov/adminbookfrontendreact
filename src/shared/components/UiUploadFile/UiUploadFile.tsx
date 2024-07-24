0d62c78f0e27940326f0e0e79825449224103be3
kirill <checkster98@gmail.com>
2024-07-24 11:14:26 +0400

    master feat(UiUploadFile): added upload-file component

---
+++ src/shared/components/UiUploadFile/UiUploadFile.tsx
@@ -0,0 +1,36 @@
+// AvatarUpload.tsx
+import {ChangeEvent, FC} from 'react';
+import {useField, useFormikContext} from 'formik';
+
+interface AvatarUploadProps {
+  name: string;
+}
+
+const AvatarUpload: FC<AvatarUploadProps> = ({name}) => {
+  const {setFieldValue} = useFormikContext();
+  const [field] = useField(name);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.currentTarget.files) {
+      setFieldValue(name, event.currentTarget.files[0]);
+    }
+  };
+
+  return (
+    <div>
+      <label htmlFor={name}>Upload Avatar</label>
+      <input id={name} name={name} type="file" onChange={handleFileChange}/>
+      {field.value && (
+        <div>
+          <img
+            src={URL.createObjectURL(field.value)}
+            alt="Avatar Preview"
+            style={{width: '100px', height: '100px', objectFit: 'cover'}}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AvatarUpload;
