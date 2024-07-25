13f9757a86c7468219531c864093333993dca17f
kirill <checkster98@gmail.com>
2024-07-25 12:03:04 +0400

    master feat(App): added new link to page

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
       <div><Link to={`/`}>main</Link></div>
       <div><Link to={`register`}>registration</Link></div>
       <div><Link to={`login`}>login</Link></div>
-
+      <div><Link to={`settings`}>Settings</Link></div>
     </div>
   )
 };
