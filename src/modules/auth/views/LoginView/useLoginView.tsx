657492e6c77c2f32bdf108b290b80cef6ff7c488
kirill <checkster98@gmail.com>
2024-07-25 12:02:02 +0400

    master fix(useLoginView): changed path to type SubmitProps

--- src/modules/auth/views/LoginView/useLoginView.tsx
+++ src/modules/auth/views/LoginView/useLoginView.tsx
@@ -2,13 +2,12 @@ import {useNavigate} from "react-router-dom";
 import {useLoginMutation} from "@/services/api/controllers/authApi";
 import {setCredentials} from "@moduleAuth/store";
 import * as Yup from "yup";
-import {ApiError} from "@/services/api/controllers/authApi/authApi.types.ts";
-import {FormProps, SubmitProps} from "@moduleAuth/views/LoginView/LoginView.types.ts";
-
-
+import {AuthErrorResponse} from "@/services/api/controllers/authApi/authApi.types.ts";
+import {FormProps} from "@moduleAuth/views/LoginView/LoginView.types.ts";
+import {SubmitProps} from "@moduleAuth/static/types";
 
 export const useLoginView = () => {
-  const [login, {isLoading, error}] = useLoginMutation();
+  const [login, {data, isLoading, error}] = useLoginMutation();
   const navigate = useNavigate();
 
   const validationSchema = Yup.object({
@@ -21,15 +20,13 @@ export const useLoginView = () => {
       const userData = await login({username: values.username, password: values.password}).unwrap();
       dispatch(setCredentials({
         accessToken: userData.accessToken,
-        refreshToken: userData.refreshToken,
-        user: userData.user,
       }));
 
       localStorage.setItem('token', userData.accessToken);
       navigate('/');
 
     } catch (error) {
-      const loginError = error as ApiError;
+      const loginError = error as AuthErrorResponse;
 
       setFieldError(loginError.data.errors[0].field, loginError.data.message)
     } finally {
@@ -41,6 +38,7 @@ export const useLoginView = () => {
     validationSchema,
     onSubmit,
     isLoading,
-    error
+    error,
+    data
   };
 };
