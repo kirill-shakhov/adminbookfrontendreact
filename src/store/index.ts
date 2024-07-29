0624e7fb401de734ecf4618111ec6ad7f0819937
kirill <checkster98@gmail.com>
2024-07-29 13:15:04 +0400

    master feat(index): added profile and book api, book slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,26 @@
 // store.ts
-import { configureStore } from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
 import authReducer from '@moduleAuth/store/index.ts';
-import { authApi } from '@/services/api/controllers/authApi/index.ts';
+import bookReducer from '@/modules/book/store/index.ts';
+import {authApi} from '@/services/api/controllers/authApi/index.ts';
+import {profileApi} from "@/services/api/controllers/profileApi";
+import {bookApi} from "@/services/api/controllers/bookApi";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    book: bookReducer,
     [authApi.reducerPath]: authApi.reducer,
+    [profileApi.reducerPath]: profileApi.reducer,
+    [bookApi.reducerPath]: bookApi.reducer
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware().concat(
+      authApi.middleware,
+      profileApi.middleware,
+      bookApi.middleware
+    )
+  ,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
