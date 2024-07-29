c947f9302173b3c884215bc7e78a356513457541
kirill <checkster98@gmail.com>
2024-07-29 13:16:09 +0400

    master feat(index): added new route

--- src/modules/settings/router/index.tsx
+++ src/modules/settings/router/index.tsx
@@ -1,6 +1,8 @@
-import {RouteObject} from "react-router-dom";
+import {Navigate, RouteObject} from "react-router-dom";
 import SettingsLayoutView from "@modules/settings/views/SettingsLayoutView/SettingsLayoutView";
+
 import PrivateRoute from "@/router/guards/PrivateRoute.tsx";
+import SettingsProfile from "@modules/settings/views/SettingsProfile/SettingsProfile";
 
 
 export default [
@@ -10,6 +12,19 @@ export default [
       permissions={['USER', 'ADMIN']}>
       <SettingsLayoutView/>
     </PrivateRoute>),
+    children: [
+      {
+        path: '',
+        element: <Navigate to="profile" replace/>,
+      },
+      {
+        path: 'profile',
+        element: (<PrivateRoute
+          permissions={['USER', 'ADMIN']}>
+          <SettingsProfile/>
+        </PrivateRoute>),
+      }
+    ]
   },
 
 ] as RouteObject[];
