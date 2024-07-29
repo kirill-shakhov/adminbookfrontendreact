51b7349f0f62a7b99d660966c48be1e3d58c0356
kirill <checkster98@gmail.com>
2024-07-29 13:15:57 +0400

    master feat(index): added _id for user

--- src/modules/auth/static/types/index.ts
+++ src/modules/auth/static/types/index.ts
@@ -3,6 +3,7 @@ import {AppDispatch} from "@/store";
 import {FormProps} from "@moduleAuth/views/LoginView/LoginView.types.ts";
 
 export interface User {
+    _id?: string;
     activationLink: string;
     email: string;
     username: string;
