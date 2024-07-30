098262413d520317e83d7274bba92a9f137e0588
kirill <checkster98@gmail.com>
2024-07-30 16:49:54 +0400

    master feat(main): added component

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,13 @@ import {store} from "@/store";
 import {Provider} from 'react-redux';
 
 import './index.css'
+import {NotificationsList} from "@/shared/components/NotificationsList";
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
+      <NotificationsList/>
       <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>,
