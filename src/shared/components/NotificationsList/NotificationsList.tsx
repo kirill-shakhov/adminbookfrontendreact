0d6e795ac1afe617dc1b8dd73ae181a5a0a75094
kirill <checkster98@gmail.com>
2024-07-30 16:42:39 +0400

    master feat(NotificationsList): added component

---
+++ src/shared/components/NotificationsList/NotificationsList.tsx
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import UiNotification from "@/shared/components/UiNotification/UiNotification.tsx";
+import {useAppSelector} from "@/store/hooks.ts";
+import {Notification} from "@modules/notifications/static/types";
+
+const NotificationsList = () => {
+
+  const notifications: Notification[] = useAppSelector((state) => state.notifications.notifications)
+
+  return (
+    <div className='fixed right-0 bottom-0 w-full max-w-xs z-50'>
+      {notifications.map((notification, index) => (
+        <UiNotification
+          key={uuidv4()}
+          type={notification.type}
+          message={notification.message}
+          index={index}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default NotificationsList;
